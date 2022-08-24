321d1be369fbd57e39c4205124ba7578f3b928b9
chenweiming <627891695@qq.com>
2022-08-24 15:12:28 +0800

    Site updated: 2022-08-24 15:12:27

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,15 @@
+#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs-dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
+date=`date +"%Y-%m-%d %H:%M:%S"`
+commitmsg='Site updated: '$date
+# yarn build
 git add -A
-git commit -m 'deploy'
+git commit -m "$commitmsg"
+git config --local user.name chenweiming
+git config --local user.email 627891695@qq.com
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
+git push -f git@github.com:chen-wm/chenwm.github.io.git master
